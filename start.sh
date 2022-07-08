860ed8316d700aa6ae4e2fda62c1e65cee8e02f8
Tellybots <99309393+Tellybots@users.noreply.github.com>
2022-07-08 11:23:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Tellybots/Vid-1.git /Vid-1   
+  git clone https://github.com/Tellybots/Vid-2.git /Vid-2  
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Vid-1
+  git clone $UPSTREAM_REPO /Vid-2
 fi
-cd /Vid-1
+cd /Vid-2
 pip3 install -U -r requirements.txt
 echo "βΣTΔ BOT IS STARTING⚡️⚡️"
 python3 -m bot
